687949df60106541e7d0ce6777a52d5445a2e4af
wanharsaniya <75015600+wanharsaniya@users.noreply.github.com>
2022-07-24 13:37:37 +0530

    Update chart2.js

--- js/chart2.js
+++ js/chart2.js
@@ -47,15 +47,15 @@ d3.csv("countries.csv",
     .attr("y", function(d) { return y(d.Country); })
     .attr("width", function(d) { return x(d.Value); })
     .attr("height", y.bandwidth() )
-    .attr("fill", function(d){ if (d.Country == 'California') return "#cc0000" ; else return "#a3a3c2"})
+    .attr("fill", function(d){ if (d.Country == 'Nevada') return "#cc0000" ; else return "#a3a3c2"})
 
 
     // Features of the annotation
 	const annotations = [
     {
     note: {
-      label: "California is 3rd highest is the third highest due to h.",
-      title: "Nevada overall sales",
+      label: "Nevada's profits were greatly influenced by the sudden increase in demand for home renovations",
+      title: "Nevada overall profit",
       wrap: 200,  // try something smaller to see text split in several lines
       padding: 10   // More = text lower
       
