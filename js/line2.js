344ea27e95f71b5ec8229bb927b4bd9c8c5a1afd
wanharsaniya <75015600+wanharsaniya@users.noreply.github.com>
2022-07-24 15:59:12 +0530

    Update line2.js

--- js/line2.js
+++ js/line2.js
@@ -103,4 +103,59 @@ d3.csv("connectedscatter.csv",
         .on("mouseover", mouseover)
         .on("mousemove", mousemove)
         .on("mouseleave", mouseleave)
+      
+      svg.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left)
+      .attr("x",0 - (height / 2))
+      .attr("dy", "1em")
+      .style("text-anchor", "middle")
+      .text("Earnings");
+      
+      var parseDate = function(d){ return d3.timeParse("%Y-%m-%d")(d)}
+      const annotations = [
+       // first annotation
+        {
+      note: {
+        label: "Earnings plummeted",
+        title: "April 17th - 19th",
+        wrap: 150,  // try something smaller to see text split in several lines
+        padding: 10   // More = text lower
+      
+     },
+     color: ["#cc0000"],
+     x: x(parseDate('2018-04-18')),
+     y: y(8197),
+     dy: -100,
+     dx: -5,
+     subject: {
+      radius: 50,
+      radiusPadding: 5
+    },
+    type: d3.annotationCalloutCircle,
+    },
+    // second annotation
+        {
+      note: {
+        label: "Strong Recovery",
+        title: "April 20th",
+        wrap: 150,  // try something smaller to see text split in several lines
+        padding: 10   // More = text lower
+      
+     },
+     color: [" #00b300"],
+     x: x(parseDate('2018-04-20')),
+     y: y(8880.23),
+     dy: 40,
+     dx: 40,
+    type: d3.annotationCalloutElbow,
+    },
+    
+      ]
+      
+      window.makeAnnotations = d3.annotation()
+        .annotations(annotations)
+     
+        svg.append("g")
+    .call(makeAnnotations)
 })
